a86e0bc8f4e7005f14ee8eb166595684763f784e
Kirill Maslov <kirjuha@gmail.com>
2025-04-21 23:25:13 +0300

    Templates for dumping waves

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def foo(arg1, arg2):
     b = arg1 - arg2
     c = arg1 * arg2
     d = arg1 / arg2
-    return a, b, c
+    return a, b, c, d
 
 e = foo(3,1)
 print (e)
@@ -14,10 +14,11 @@ attr = {'arg1': {'signed': 0, 'width': 8},
         'arg2': {'signed': 0, 'width': 8},
         'a': {'signed': 0, 'width': 9},
         'b': {'signed': 1, 'width': 9},
-        'c': {'signed': 0, 'width': 9},}
+        'c': {'signed': 0, 'width': 9},
+        'd': {'signed': 0, 'width': 9},}
 
 foo_tb = Py2ver(foo, attr).tb()
 
-g = foo_tb(1,3)
+g = foo_tb(4,4)
 print(g)
 
